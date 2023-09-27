dbe929cb31f10a0d9fdf86f3e5f2f8d7f870280f
y1np1 <142690380+y1np1@users.noreply.github.com>
2023-09-27 11:06:05 -0300

    script.js

--- script.js
+++ script.js
@@ -33,6 +33,25 @@ function calculateTotalPrice() {
     }
 }
 
+// Function to calculate and display total sales amount as an integer
+function calculateTotalSalesAmount() {
+    const orders = JSON.parse(localStorage.getItem("orders")) || {};
+    let totalSalesAmount = 0;
+
+    // Calculate total sales amount as an integer
+    orders.forEach(order => {
+        const precio = parseFloat(order.precio);
+
+        if (!isNaN(precio)) {
+            totalSalesAmount += Math.round(precio);
+        }
+    });
+
+    // Display the total sales amount as an integer
+    const totalSalesAmountSpan = document.getElementById("total-sales-amount");
+    totalSalesAmountSpan.textContent = totalSalesAmount;
+}
+
 // Function to view order history for a person
 function viewOrderHistory(nombre) {
     const orders = JSON.parse(localStorage.getItem("orders")) || [];
@@ -52,6 +71,14 @@ function viewOrderHistory(nombre) {
         const listItem = document.createElement("li");
         listItem.innerHTML = `<strong>Número de Prenda:</strong> ${order.numeroPrenda}<br>
                               <strong>Precio:</strong> ${order.precio}`;
+        
+        // Create a button to remove the order
+        const removeButton = document.createElement("button");
+        removeButton.textContent = "Borrar Prenda";
+        removeButton.className = "remove-order-button";
+        removeButton.addEventListener("click", () => removeOrder(nombre, order.numeroPrenda));
+        listItem.appendChild(removeButton);
+
         orderList.appendChild(listItem);
     });
 }
@@ -85,6 +112,7 @@ function addOrder() {
 
         // Recalculate and display total prices
         calculateTotalPrice();
+        calculateTotalSalesAmount(); // Call this to update the total sales amount as well
     }
 }
 
@@ -94,10 +122,38 @@ function clearHistory() {
     // Clear the displayed order list
     const orderList = document.getElementById("order-list");
     orderList.innerHTML = "";
+
+    // Reset the total sales amount
+    const totalSalesAmountSpan = document.getElementById("total-sales-amount");
+    totalSalesAmountSpan.textContent = "0"; // Set it to zero
 }
 
-// Initial calculation and display of total prices
+// Function to remove an order from a person's history
+function removeOrder(nombre, numeroPrenda) {
+    const orders = JSON.parse(localStorage.getItem("orders")) || [];
+    
+    // Find the index of the order to remove
+    const indexToRemove = orders.findIndex(order => order.nombre === nombre && order.numeroPrenda === numeroPrenda);
+    
+    if (indexToRemove !== -1) {
+        // Remove the order from the array
+        orders.splice(indexToRemove, 1);
+        
+        // Save the updated order list to local storage
+        localStorage.setItem("orders", JSON.stringify(orders));
+        
+        // Refresh the order history for the selected person
+        viewOrderHistory(nombre);
+        
+        // Recalculate and display total prices and total sales amount
+        calculateTotalPrice();
+        calculateTotalSalesAmount();
+    }
+}
+
+// Initial calculation and display of total prices and total sales amount
 calculateTotalPrice();
+calculateTotalSalesAmount();
 
 function generateTxtFile() {
     const orders = JSON.parse(localStorage.getItem("orders")) || [];
